c360d1d2b0d85886ac3097aae46ac173df35e685
Maureen Murphy <21282884+AlpenMo@users.noreply.github.com>
2024-07-25 11:36:54 -0600

    Add answers to final Javascript questions
    This commit adds the answers to Javascript Excercise questions
    08 - 12.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let cleanedUp = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    let reverseString = cleanedUp.split("").reverse().join("");
+    if (cleanedUp === reverseString) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    if (people.length === 0) return null;
+
+    let now = new Date();
+    let currentYear = now.getFullYear();
+   
+    return people.reduce((oldest, current) => {
+        const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+        const currentAge = (current.yearOfDeath || currentYear) - current.yearOfBirth;
+        return currentAge > oldestAge ? current : oldest;
+    });
 
 };
 
